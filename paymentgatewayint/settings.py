9637a85a696ce526aafefba6333fdadea826782d
Bipul Vishwakarma <76177872+Bipulvishwakarma@users.noreply.github.com>
2021-02-19 17:33:19 +0530

    secret key changed

--- paymentgatewayint/settings.py
+++ paymentgatewayint/settings.py
@@ -20,7 +20,7 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ax#x)1h3j0)@kc#-0--6gc+=97%87u%0ybc3tgc@i6i&x%jdzk'
+SECRET_KEY = 'ax#x)1h3j0)@kc#-0--6gc+=97%87u%0ybc3tgc@i6i&x%jdzksds'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
